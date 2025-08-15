3e66cba1fe77f04962c9e29a17646147f30375dd
Weiyi Kong <kongwy@users.noreply.github.com>
2025-08-15 15:29:15 +1000

    initial commit

---
+++ Rayford/RayfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayfordApp.swift
+//  Rayford
+//
+//  Created by Weiyi Kong on 15/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct RayfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RayfordTests/RayfordTests.swift
@@ -0,0 +1,17 @@
+//
+//  RayfordTests.swift
+//  RayfordTests
+//
+//  Created by Weiyi Kong on 15/8/2025.
+//
+
+import Testing
+@testable import Rayford
+
+struct RayfordTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
