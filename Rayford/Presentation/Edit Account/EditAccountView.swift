6e18c513e743a11fad3691ec3e82f9daf8cab53c
Weiyi Kong <kongwy@users.noreply.github.com>
2025-08-26 21:07:28 +1000

    feat: EditAccountView

---
+++ Rayford/Presentation/Edit Account/EditAccountView.swift
@@ -0,0 +1,64 @@
+//
+//  EditAccountView.swift
+//  Rayford
+//
+//  Created by Weiyi Kong on 26/8/2025.
+//
+
+import SwiftUI
+
+struct EditAccountView: View {
+    @EnvironmentObject var store: Store
+    @StateObject var viewModel: ViewModel
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        Form {
+            Section("Display") {
+                TextField("Name", text: $viewModel.name)
+                TextField("Issuer", text: $viewModel.issuer)
+            }
+        }
+        .scrollDismissesKeyboard(.interactively)
+        .toolbar {
+            ToolbarItem(placement: .cancellationAction) {
+                Button("Cancel") {
+                    dismiss()
+                }
+            }
+            ToolbarItem(placement: .confirmationAction) {
+                Button("Done") {
+                    viewModel.save(in: store)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+extension EditAccountView {
+    class ViewModel: ObservableObject {
+        private let id: UUID
+        @Published var name: String
+        @Published var issuer: String
+
+        init(account: Account) {
+            self.id = account.id
+            self.name = account.name ?? ""
+            self.issuer = account.issuer ?? ""
+        }
+
+        func save(in store: Store) {
+            store.appState.accounts.update(id: id) { account in
+                account.name = name
+                account.issuer = issuer
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        EditAccountView(viewModel: .init(account: mockState.accounts.randomElement()!))
+    }
+}
